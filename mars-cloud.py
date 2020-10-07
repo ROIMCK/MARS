504961ce220b571495f250a6065fa503d6907c26
Grant Moyle <Grant.A.Moyle@roitraining.com>
2020-10-07 18:01:53 +0000

    Update

--- mars-cloud.py
+++ mars-cloud.py
@@ -25,7 +25,7 @@ def run():
     ]
 
     p = beam.Pipeline(argv=argv)
-    input = 'gs://moonbank-mars-sample/*.csv'
+    input = 'gs://roimck-mars-sample/*.csv'
     output = 'gs://' + bucketname + '/output/output'
 
     (p
