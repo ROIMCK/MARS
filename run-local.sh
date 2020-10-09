bc48966b786e1f856923709a31ae63cbcb719a4f
GrantAMoyle <Grant.A.Moyle@roitraining.com>
2020-10-09 08:12:20 -0500

    Update run-local.sh

--- run-local.sh
+++ run-local.sh
@@ -1,5 +1,13 @@
-# MAKE SURE PROJECT IS SET
+# MAKE SURE YOUR PROJECT IS SET
 # gcloud config set project PROJECT_ID
+# NOTE: this will set $GOOGLE_CLOUD_PROJECT environment variable
+# REQUIRES bucket names gs://$GOOGLE_CLOUD_PROJECT-bucket
+# Command to create: gsutil mb gs://$GOOGLE_CLOUD_PROJECT"-bucket"
+# REQUIRES bigquery dataset mars
+# Command to create: bq mk mars
+# REQUIRES bigquery table mars.activities
+# Command to create: bq mk --schema timestamp:STRING,ipaddr:STRING,action:STRING,srcacct:STRING,destacct:STRING,amount:NUMERIC,customername:STRING -t mars.activities
+
 echo $GOOGLE_CLOUD_PROJECT
 rm -R output
 mkdir output
